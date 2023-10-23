b9b30dc8776d84e68396c80584b29adb2f0e1a82
Yuvraj Rathva <rathva.1@iitj.ac.in>
2023-10-23 14:12:14 +0530

    basic working for one connection

---
+++ socket/run.py
@@ -0,0 +1,13 @@
+# Unix sockets are used to communicate between processes on the same machine.
+
+if __name__ == '__main__':
+    from .transports.unixsocket import UNIXSocketTransport
+
+    transport = UNIXSocketTransport()
+
+    import sys # which provides access to some variables used or maintained by the Python interpreter and to functions that interact strongly with the interpreter, including the command-line arguments.
+
+    if sys.argv[1] == 'server': # sys.argv is a list in Python, which contains the command-line arguments passed to the script.
+        transport.run_server() #start a Unix socket server
+    else:
+        print(transport.run_cmd(sys.argv[1]).to_json(indent=4)) 
